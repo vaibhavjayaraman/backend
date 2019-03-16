b17c3f8030ed63d5f5a2980fb8c83cca7f485c4b
Vaibhav <vaibhavjayaraman@gmail.com>
2019-03-16 02:00:34 -0700

    new direction

---
+++ pkg/articledata/datalookup.go
@@ -0,0 +1,33 @@
+package datalookup
+
+//create gRPC for lat/lon and have postgres instance for database be named something else
+//https://docs.microsoft.com/en-us/sql/relational-databases/spatial/query-spatial-data-for-nearest-neighbor?view=sql-server-2017
+
+/**
+have / handle lat/lon
+		- pass off lat/lon to message bus so that "trending" can be generated via mypy microservice
+		- store lat/lon points generated at a later time (too much data otherwise)
+-have /custommarker will add custom marker to the datastore
+
+- golang database should be stupid. Just has the url, title, and some extract which it will continue to serve
+- will also have a routine that looks to update underlying postgres via messagebus (and grpc)
+- if cache miss (cannot find anything within 1000 kilometers (but work on this heuristic including a null marker) call wikipedia api yourself.
+	- When passing off lat/lon via gRPC also note that
+
+kafka - keeps master copy of article database
+- once updated, then sends information via grpc to store in cached postgis database
+
+-another golang service (datainteraction)
+	- have content
+	- have /error handle errors
+	- have /interaction handle likes, times generated, times hovered over, and times clicked on - pass off to message bus
+
+- golang service for recommendations (recommendations)
+	- has own database with user recommendations - waits on pushes from central database
+**/
+
+/**
+gRPC
+	1)
+		lat/lon/database miss
+**/
